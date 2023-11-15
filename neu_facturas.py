b34a1a068d9b269e618253751ce76a9f8368cbcd
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-11-15 17:07:47 -0500

    Create neu_facturas.py

---
+++ neu_facturas.py
@@ -0,0 +1,17 @@
+def get_fact_notif(url,id=2219, From:str="2023-01-01T00:00:00", To:str="2023-12-31T00:00:00"):
+    try:
+        response = requests.post(url, headers=headers,json = {"Id_service": id,"From": From,  "To": To})
+        print(response)
+        myjson = response.json()
+        maxPage = myjson['maxPage']
+        data = []
+        data.extend(myjson['data'])
+
+        if maxPage > 1:
+            for i in range(2,maxPage+1):
+                response = requests.post(url+"?page={PagenNumber}".format(PagenNumber=i), headers=headers,json = {"Id_service": id,"From": From,  "To": To})
+                myjson = response.json()
+                data.extend(myjson['data'])
+        return data
+    except:
+        print("No se obtuvieron datos")
