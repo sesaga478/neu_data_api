a608ec57d2310748c9e0d1d3c1db3be03b843cc3
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-11-16 11:09:06 -0500

    Update neu_accounts_serv_neugets.py

--- neu_accounts_serv_neugets.py
+++ neu_accounts_serv_neugets.py
@@ -1,5 +1,6 @@
 import requests
 def get_acc_serv_neug(token,url,id):
+  #id ==> integer from id_account
   headers= {"Authorization": f"Bearer {token}"}
   url=url+str(id)
   response = requests.get(url, headers=headers)
